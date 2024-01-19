89ab0c1e7ce90688a7775792abfae22fd0069629
Sujan TR <130475138+Sujan326@users.noreply.github.com>
2024-01-19 15:50:38 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -52,14 +52,6 @@ def takeCommand():
         return "None"
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
-
 if __name__ == "__main__":
     wishMe()
     while True:
@@ -88,20 +80,12 @@ if __name__ == "__main__":
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
 
-        elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
+        elif 'open spotify' in query:
+            webbrowser.open("spotify.com")
 
-        # elif 'email to harry' in query:
-        #     try:
-        #         speak("What should I say?")
-        #         content = takeCommand()
-        #         to = "harryyourEmail@gmail.com"    
-        #         sendEmail(to, content)
-        #         speak("Email has been sent!")
-        #     except Exception as e:
-        #         print(e)
-        #         speak("Sorry my friend harry bhai. I am not able to send this email")    
+        elif "thank you" in query:
+            speak("Welcome")
+             
         else:
             print("No query matched")
             print("No query matched")
